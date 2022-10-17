7db610272e4c353e2d55a3fa9f1c20b54d662b90
Ulises Umansky <uumansky@gmail.com>
2022-10-17 18:56:46 +0300

    general_frame and menu_panel classes

--- Index.py
+++ Index.py
@@ -1,11 +1,10 @@
-
 from tkinter import *
 from tkinter import ttk
 
 
 class Menu_panel(Frame):
     def __init__(self, container):
-        super().__init__(container, highlightbackground='blue', highlightthickness=4)
+        super().__init__(container, highlightbackground='red', highlightthickness=4)
 
         self.label = Label(self, text='im a menu panel')
         self.label.pack(expand=True)
@@ -13,10 +12,11 @@ class Menu_panel(Frame):
         self.pack(side=LEFT)
 
 
-class General_panel(Frame):
-    def __init__(self, container, side):
-        super().__init__(container, bg='black', padx=10, pady=10)
-        self.pack(side=side, fill=Y)
+class General_frame(Frame):
+    def __init__(self, container, side, **kwargs):
+        super().__init__(container, kwargs, highlightbackground='blue', highlightthickness=4)
+        self.pack(side=side)
+
 
 class Window(Tk):
     def __init__(self):
@@ -27,12 +27,16 @@ class Window(Tk):
 
 def main():
     app = Window()
-    top_panel = General_panel(app, TOP)
-    left_panel = General_panel(app, LEFT)
-    label = Label(top_panel, text='top panel')
-    menu1 = Menu_panel(left_panel)
-    menu2 = Menu_panel(left_panel)
-    menu3 = Menu_panel(left_panel)
+    top_frame = General_frame(app, TOP, bg='black')
+    left_frame = General_frame(app, LEFT, bg='black')
+    right_frame = General_frame(app, RIGHT, bg='black')
+    label = Label(top_frame, text='top panel')
+    label.pack()
+    label = Label(right_frame, text='right panel', anchor='center', width=100)
+    label.pack()
+    menu1 = Menu_panel(left_frame)
+    menu2 = Menu_panel(left_frame)
+    menu3 = Menu_panel(left_frame)
 
     app.mainloop()
 
